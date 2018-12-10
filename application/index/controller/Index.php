2c54258d8ef7c963464e500eb5a92ad849ad896c
yang <651629811@qq.com>
2018-12-10 23:25:53 +0800

    引入Auth组件 权限分配
    设置用户组，用户，权限 完成按不同权限名加载左侧导航栏

---
+++ application/index/controller/Index.php
@@ -0,0 +1,40 @@
+<?php
+namespace app\index\controller;
+use think\Auth;
+use think\Controller;
+use think\Db;
+
+class Index extends Controller
+{
+    public function index(){
+        $auth = new Auth();
+        $groups = $auth->getGroups(1);
+        $groupsInfo = Db::name('auth_rule')->where('id','in',explode(',',$groups[0]['rules']))->select();
+        $tree = [];
+        //筛选出结果集中的所有主节点
+        foreach ($groupsInfo as $key=>$value){
+            if($value['nid'] == 0){
+                $tree[] = $value;
+            }
+        }
+
+        //从结果集中筛选出所有子节点 并插入到对应的父节点中
+        foreach($tree as $treeKey=>$treeValue){
+            foreach ($groupsInfo as $sonKey=>$sonValue){
+                if($sonValue['nid'] == $treeValue['id']){
+                    $tree[$treeKey]['childrens'][] = $sonValue;
+                }
+            }
+
+        }
+        //页面输出
+        return $this->fetch('',[
+            'tree' => $tree
+        ]);
+    }
+
+    public function son(){
+        return '12';
+
+    }
+}
